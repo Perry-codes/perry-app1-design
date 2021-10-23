106e27661fa9939931aa5b7ef9626ce52d4698df
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-10-23 00:30:46 -0400

    add basic gui fxml

--- src/main/java/baseline/Application.java
+++ src/main/java/baseline/Application.java
@@ -1,8 +1,24 @@
 package baseline;
 
-public class Application {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
 
     public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ToDoListGui.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("To Do List");
+        stage.setScene(scene);
+        stage.show();
     }
 }
