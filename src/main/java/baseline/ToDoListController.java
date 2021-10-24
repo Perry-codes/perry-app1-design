e0e4f3ecb450393bc054bb9804c9f5a0e1f0e217
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-10-24 18:09:50 -0400

    gui update

--- src/main/java/baseline/ToDoListController.java
+++ src/main/java/baseline/ToDoListController.java
@@ -1,4 +1,28 @@
 package baseline;
 
 public class ToDoListController {
+
+    //Controller performs the actions desired when gui interface is used
+
+    //edit > export
+        //save out list to file
+    //edit > import
+        //import lists from file
+
+    //List View
+        //will show To Do lists
+            //Clicking a list will show the tasks and dues dates on that list on the right table view
+        //click list to select and hit delete to delete list
+            //pop up to confirm?
+        //click list to elect and hit add task for new task
+            //pop up to input task desc and duw date?
+
+    //Table view
+        //will show the tasks and due dates of the active to do list
+        //click list item to select and hit complete to mark complete
+            //  show complete items with strike through text
+                //use a .css file to do this?
+        //Add task will add a task to the active list
+        //click list item to select and delete to delete item from list
+
 }
